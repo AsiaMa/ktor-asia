282a6172e2f1a43940abed4228d7d6be99d9ff26
mayazhou <mayazhou@oasgames.com>
2021-11-17 19:42:49 +0800

    feat: print uri

--- src/main/kotlin/com/asia/plugins/Routing.kt
+++ src/main/kotlin/com/asia/plugins/Routing.kt
@@ -5,22 +5,28 @@ import io.ktor.features.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import org.slf4j.event.Level
 
 fun Application.configureRouting() {
+    // add Server and Date headers into each response
+    /*
+       Date: Wed, 17 Nov 2021 11:33:30 GMT
+       Server: ktor-server-core/1.6.5
+    */
     install(DefaultHeaders)
     install(CallLogging) {
-        level = Level.DEBUG
+        // 改变日志的级别 default: Level.INFO
+//        level = Level.TRACE
 
         filter { call ->
-            call.request.path().startsWith("/api")
+            call.request.path().startsWith("/he")
         }
 
         format { call ->
+            val uri = call.request.uri
             val status = call.response.status()
             val httpMethod = call.request.httpMethod.value
             val userAgent = call.request.headers["User-Agent"]
-            "Status: $status, HTTP method: $httpMethod, User agent: $userAgent"
+            "uri: $uri, Status: $status, HTTP method: $httpMethod, User agent: $userAgent"
         }
 
         mdc("name-parameter") { call ->
